16ec9e1a20f3441d4a20cae71221537936a15a21
benjyz <benjamin.l.cordes@gmail.com>
2015-12-02 11:05:50 +0100

    init readme and netty

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Notary
+
+The Bitcoin Blockchain can be seen as a notary service, since it is a highly reliably public datastore
+and implements a tagging of meta-data (OP_RETURN).
+
+Notary is an abstraction over Bitcoin to issue and transfer assets, currently mostly based around OpenAssets.
---
+++ src/main/java/EchoServerHandler.java
@@ -0,0 +1,19 @@
+
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+
+public class EchoServerHandler extends ChannelHandlerAdapter {
+
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ctx.writeAndFlush(msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
