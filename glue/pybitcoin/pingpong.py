8d2fc0a6d3695bc6a83a330204eabfd3d93675c5
benjyz <benjamin.l.cordes@gmail.com>
2015-12-06 10:57:07 +0100

    send as function

--- glue/pybitcoin/pingpong.py
+++ glue/pybitcoin/pingpong.py
@@ -7,14 +7,16 @@ example how to send from one brainwallet to another
 
 from bitcoin import *
 
-def getwallet(pw):
-    """ simple wallet structure """
-    priv = sha256(pw)
+fee = 10000
+
+def getwallet(password):
+    """ simple brainwallet structure """
+    priv = sha256(password)
     pub = privtopub(priv)
     addr = pubtoaddr(pub)
     h = history(addr)
     unspent = bci_unspent(addr)
-    balance = calc_total(h)
+    balance = calc_total(unspent)
     return {'priv':priv, 'pub':pub, 'addr':addr, 'hist':h, 'unspent': unspent, 'balance':balance}
 
 def calc_total(h):
@@ -24,73 +26,49 @@ def calc_total(h):
         total += v
     return total
 
-wallet1 = getwallet('Lykkex1')
-wallet2 = getwallet('Lykkex2')
-print 'Lykkex1\n',wallet1
-print 'Lykkex2\n',wallet2
+def sendbtc(fromwallet, towallet):
+    outs = list()
+    fromwallet = wallet1
+    towallet = wallet2
 
-#========================================
-#print tx
-#sign first input
-#tx2 = sign(tx,0,wallet1['priv'])
-#rint tx2
+    unspent = bci_unspent(fromwallet['addr'])
+    #spend the first output
+    inputindex = 0
+    print unspent
+    spendoutput = unspent[inputindex]
+    #print spendoutput
 
-#from wallet1 to wallet2
-fromaddr = wallet2['addr']
+    outval = spendoutput['value'] - fee #totalout
+    outs = [{'value': outval, 'address': towallet['addr']}]
+    print outs
+    tx = mktx(spendoutput,outs)
+    signedtx = sign(tx,0,fromwallet['priv'])
+    print signedtx
+    #print bci_pushtx(signedtx)
 
-outs = list()
-towallet = wallet1
-fromwallet = wallet2
+    #tx = mktx(fromwallet['hist'],outs)
+    #TODO: gather all outputs from the address
 
-h = history(fromwallet['addr'])
-#print h
-totalout = calc_total(h)
-#print totalout
-
-unspent = bci_unspent(fromwallet['addr'])
-
-#spend the first output
-spendoutput = unspent[0]
-#print spendoutput
-
-outval = spendoutput['value'] #totalout
-outs = [{'value': outval, 'address': towallet['addr']}]
-
-tx = mktx(spendoutput,outs)
-signedtx = sign(tx,0,fromwallet['priv'])
-
-#print bci_pushtx(signedtx)
-
-#tx = mktx(fromwallet['hist'],outs)
-
-
-"""
-for o in bci_unspent(fromwallet):
-    print o
-    outs.append({'value': o['value'], 'address': toaddr})
-"""
-#list of previous outputs as new inputs
-"""
-inputindex = 0
-txall = None
-pkey = wallet1['priv']
-for newinput in outs:
-    sign(tx,inputindex,pkey)
-    inputindex+=1
-"""
-
-#sign(tx,0,wallet2['priv'])
+    """
+    for o in bci_unspent(fromwallet):
+        print o
+        outs.append({'value': o['value'], 'address': toaddr})
+    """
+    #list of previous outputs as new inputs
+    """
+    inputindex = 0
+    txall = None
+    pkey = wallet1['priv']
+    for newinput in outs:
+        sign(tx,inputindex,pkey)
+        inputindex+=1
+    """
 
 
-"""
-======
-output
-======
-Lykkex1
-{'hist': [{'output': u'e4cce6e5cf0b2c3a0b95b8ff124a900757861b3ac722844aa6eab968935d0b97:0', 'block_height': 386667, 'value': 100000, 'address': u'13TZJXS5w6NoFUn9vwDJgMBDnTh2CQAz8Y'}], 'addr': '13TZJXS5w6NoFUn9vwDJgMBDnTh2CQAz8Y', 'pub': '0435baa6822c6ad593cd33d1f33bca15f4ba0ad8fcc2369d7c1b541e4a05e6e5cb2b10a1fd6d85c6840e2b8f951c600a2ab5f147c5b5e312ad03543ed930a3fda5', 'priv': '16f9c43035061fda0fd54817d0ce075eaeec0cc6ba1e4e3ff207fbc847b3afd0'}
-Lykkex2
-{'hist': [{'output': u'9a3797d92202ec53c16c21a600b899fdfab10c1dcecd7a5b1cee89a78d198bcd:0', 'block_height': 386680, 'value': 100000, 'address': u'15mfUNVsf1WUAbgvv5NfWsAbGheyHt5M3L'}], 'addr': '15mfUNVsf1WUAbgvv5NfWsAbGheyHt5M3L', 'pub': '04306ebfd54a65f7c43caf4bfc22fa155494160c7cb5df1fd720e16dd30a9716a892574467113afeb605520fb3aa84bfe232f559ca2692713fcb2a6cebc0b16ebe', 'priv': 'd270c73975a23dcb4e0ed24bc5cdce99e24e3689d23952ece15926196b34bdd9'}
-0100000001970b5d9368b9eaa64a8422c73a1b865707904a12ffb8950b3a2c0bcfe5e6cce40000000000ffffffff0128230000000000001976a9143452eb91417c48324ce4dfcfb6597ecfdeefc5b488ac00000000
-0100000001970b5d9368b9eaa64a8422c73a1b865707904a12ffb8950b3a2c0bcfe5e6cce4000000008a4730440220088b1da7d907eda5e5d1ddabd9cf1b76226a0a656082bf896a66f2a26e9cd80a02201907d6f02325a1a835df644e545eed012f3f6b3304ace75c497f3ad9b15565f801410435baa6822c6ad593cd33d1f33bca15f4ba0ad8fcc2369d7c1b541e4a05e6e5cb2b10a1fd6d85c6840e2b8f951c600a2ab5f147c5b5e312ad03543ed930a3fda5ffffffff0128230000000000001976a9143452eb91417c48324ce4dfcfb6597ecfdeefc5b488ac00000000
-Transaction Submitted
-"""
+if __name__ =='__main__':
+    wallet1 = getwallet('Lykkex1')
+    wallet2 = getwallet('Lykkex2')
+    #print 'Lykkex1\n',wallet1
+    #print 'Lykkex2\n',wallet2
+    print '========================================'
+    sendbtc(wallet1, wallet2)
